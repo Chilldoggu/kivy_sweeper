bec8895a4372d858b11bcb5712551768e80929f0
Chilldog <zerooneskyline@gmail.com>
2023-07-16 10:50:54 +0200

    some minor improvements

--- GUI.py
+++ GUI.py
@@ -5,18 +5,20 @@
 from kivy.uix.screenmanager import ScreenManager, Screen, FadeTransition
 from kivy.uix.button import Button
 
-from kivy.properties import ObjectProperty, ListProperty
+from kivy.properties import ObjectProperty, ListProperty, NumericProperty
 from kivy.config import Config
 from kivy.clock import Clock
 from kivymd.app import MDApp
+from kivy.factory import Factory
+from kivy.core.window import Window
 
 import random
 # import time
 
 # TOOD:
-# - Lose screen
-# - Win screen
-# - Timer
+# + Lose screen
+# + Win screen
+# + Timer
 
 Config.set("graphics", "resizable", False)
 Config.set("graphics", "width", 1280)
@@ -41,14 +43,14 @@ class Minesweeper:
     def __init__(self, board_size: tuple, difficulty=0.1) -> None:
         self.player_move = 0
         self.board_size = board_size
+        self.squares = board_size[0] * board_size[1]
         self.mine_amount = int(difficulty * self.board_size[0] * self.board_size[1])
+        self.revealed_squares = 0
 
         self.create_grid(self.mine_amount)
 
     def create_grid(self, mine_amount: int) -> None:
         self.logic_structure = []
-        # Not used for GUI gameplay, only for console output
-
         # Make empty grid
         for row in range(self.board_size[1]):
             self.logic_structure.append([])
@@ -96,6 +98,7 @@ class Minesweeper:
 class GameplaySquare(ScreenManager):
     btnWidgetObj = ObjectProperty(None)
     imgWidgetObj = ObjectProperty(None)
+    gameObj = ObjectProperty(None)
 
     def __init__(self, **kwargs):
         super(GameplaySquare, self).__init__(**kwargs)
@@ -106,71 +109,108 @@ class GameplaySquare(ScreenManager):
 
 
 class GameplaySquareButton(Screen):
-    squareBtnObj = ObjectProperty(None)
-
-    def on_left_click(self):
-        # Give out the coordinates of a button in a grid to a Game.check_square()
-        for widget in self.manager.parent.walk_reverse():
-            if widget.__class__.__name__ == "Game":
-                [
-                    widget.check_square([y, row.index(self.manager)])
-                    for y, row in enumerate(widget.ButtonList)
-                    if self.manager in row
-                ]
+    ...
 
 
 class InnerGameplaySquareButton(Button):
     flag = False
 
+    # NANI DA FACK THE INDENTATION DOIN???????
     def on_touch_up(self, touch):
         if self.collide_point(*touch.pos) and touch.grab_current is self:
-            if touch.button == "right":
-                if self.flag:
-                    self.background_normal = 'button_normal.png'
-                    self.background_down = 'button_down.png'
-                else:
-                    self.background_normal = 'button_flag.png'
-                    self.background_down = 'button_flag.png'
-                self.flag = not self.flag
-            elif touch.button == "left" and not self.flag:
-                self.parent.on_left_click()
+            manager = self.parent.manager
+            for widget in manager.parent.walk_reverse():
+                if widget.__class__.__name__ == "Game":
+                    if touch.button == "right":
+                        if self.flag:
+                            widget.total_flags -= 1
+                            self.background_normal = 'button_normal.png'
+                            self.background_down = 'button_down.png'
+                        else:
+                            widget.total_flags += 1
+                            self.background_normal = 'button_flag.png'
+                            self.background_down = 'button_flag.png'
+                        self.flag = not self.flag
+                    elif touch.button == "left" and not self.flag:
+                        for y, row in enumerate(widget.ButtonList):
+                            if manager in row:
+                                widget.check_square([y, row.index(manager)])
         return super(Button, self).on_touch_up(touch)
 
 
 class GameplaySquareImage(Screen):
-    def on_left_click(self):
-        for widget in self.manager.parent.walk_reverse():
-            if widget.__class__.__name__ == "Game":
-                [
-                    widget.combo_reveal([y, row.index(self.manager)]) for y, row in enumerate(widget.ButtonList) if self.manager in row
-                ]
-
     def on_touch_up(self, touch):
         if self.collide_point(*touch.pos):
             if touch.button == "right":
                 ...
             elif touch.button == "left":
-                self.on_left_click()
+                for widget in self.manager.parent.walk_reverse():
+                    if widget.__class__.__name__ == "Game":
+                        [
+                            widget.combo_reveal([y, row.index(self.manager)])
+                            for y, row in enumerate(widget.ButtonList)
+                            if self.manager in row
+                        ]
         return super(Screen, self).on_touch_up(touch)
 
 
 class Game(Screen):
+    mainBoxLayoutObj = ObjectProperty(None)
     gameplayGridObj = ObjectProperty(None)
     gameplaySquareObj = ObjectProperty(None)
+    mineCountdownObj = ObjectProperty(None)
+    timeCountdownObj = ObjectProperty(None)
     ButtonList = ListProperty(None)
-    mineSweeperObj = ObjectProperty(Minesweeper(board_size=(12, 10)))
+    mineSweeperObj = ObjectProperty(None)
+    total_flags = NumericProperty(None)
+    time = NumericProperty(None)
 
-    def __init__(self, **kw):
+    def __init__(self, *args, **kw):
+        self.clear_widgets()
         super(Game, self).__init__(**kw)
+        self.total_flags = 0
+        self.time = 0
+        self.mineSweeperObj = Minesweeper(board_size=(12, 10))
         Clock.schedule_once(lambda dt: self.make_grid())
+        Clock.schedule_once(lambda dt: self.set_mine_countdown(self.mineSweeperObj.mine_amount - self.total_flags))
+        self.timeCountdownEvent = Clock.schedule_interval(lambda dt: self.update_time_countdown(), 1)
+
+    def set_mine_countdown(self, mines_left):
+        charLabels = reversed(self.mineCountdownObj.children[:-1])
+        flag_chars = f"{mines_left}".zfill(3)
+        if len(flag_chars) <= 3:
+            for i, charLabel in enumerate(charLabels):
+                charLabel.text = flag_chars[i]
+
+    def update_time_countdown(self):
+        self.time += 1
+        charLabels = reversed(self.timeCountdownObj.children[:-1])
+        time_chars = f"{self.time}".zfill(3)
+        if len(time_chars) <= 3:
+            for i, charLabel in enumerate(charLabels):
+                charLabel.text = time_chars[i]
 
     def make_grid(self):
         self.gameplayGridObj.clear_widgets()
         self.ButtonList = []
-
         # Show the logic structure of the gamefield
         for row in self.mineSweeperObj.logic_structure:
             print(row)
+        print()
+
+        # Set up GUI grid size properties
+        board_size = self.mineSweeperObj.board_size
+        self.gameplayGridObj.rows = board_size[1]
+        self.gameplayGridObj.cols = board_size[0]
+        if board_size[0] >= 12 or board_size[1] >= 10:
+            self.gameplayGridObj.row_default_height = 68.0
+            self.gameplayGridObj.col_default_width = 68.0
+        elif board_size[0] > board_size[1]:
+            self.gameplayGridObj.col_default_width = (Window.size[0] * .6) / board_size[0]
+            self.gameplayGridObj.row_default_height = self.gameplayGridObj.col_default_width
+        else:
+            self.gameplayGridObj.row_default_height = (Window.size[1] - self.mainBoxLayoutObj.padding[0] * 2) / board_size[1]
+            self.gameplayGridObj.col_default_width = self.gameplayGridObj.row_default_height
 
         # Set up the GUI grid with buttons and proper images (1, 2, mine, etc...)
         for i in range(self.gameplayGridObj.rows):
@@ -188,19 +228,24 @@ class Game(Screen):
         current_mark = self.ButtonList[y][x].imgWidgetObj.children[0].source[:-4]
         self.ButtonList[y][x].current = "image"
 
+        self.mineSweeperObj.player_move += 1
+        # Start recursion to open big empty spaces
         if current_mark == "X":
             # Restart grid if first move is a mine
             if self.mineSweeperObj.player_move == 1:
                 self.mineSweeperObj.player_move = 0
-                self.mineSweeperObj.create_grid(self.mineSweeperObj.mine_amount)
-                return self.check_square(cords)
+                self.__init__()
             else:
-                print("YOU LOST")
-        # Start recursion to open big empty spaces
+                self.on_lose()
+        else:
+            self.mineSweeperObj.revealed_squares += 1
+            if self.mineSweeperObj.revealed_squares == self.mineSweeperObj.squares - self.mineSweeperObj.mine_amount:
+                self.on_win()
         if current_mark == "0" and recursion:
             self.uncheck_outer_square(y, x)
 
-    def uncheck_outer_square(self, row, column, flag_search=False) -> bool:
+    def uncheck_outer_square(self, row, column, flag_search=False):
+        flag_counter = 0
         for loop_y in range(-1, 2):
             for loop_x in range(-1, 2):
                 out_y = row + loop_y
@@ -212,18 +257,36 @@ class Game(Screen):
                     if squareObj.current == "button" and \
                             not squareObj.get_screen('button').children[0].flag and \
                             not flag_search:
-                        squareObj.current = "image"
                         self.check_square(cords=[out_y, out_x])
-                    if squareObj.current == "button" and flag_search and squareObj.get_screen('button').children[0].flag:
-                        return True
+                    if squareObj.current == "button" and \
+                            flag_search and \
+                            squareObj.get_screen('button').children[0].flag:
+                        flag_counter += 1
+                        continue
+        if flag_search:
+            return flag_counter
         return False
 
     def combo_reveal(self, cords):
         y = cords[0]
         x = cords[1]
-        if not self.uncheck_outer_square(y, x, flag_search=True):
+        flag_counter = self.uncheck_outer_square(y, x, flag_search=True)
+        curr_val = int(self.mineSweeperObj.logic_structure[y][x])
+        if not flag_counter == curr_val:
             return None
         self.uncheck_outer_square(y, x)
+
+    def on_lose(self):
+        self.timeCountdownEvent.cancel()
+        popup = Factory.LosePopup()
+        popup.bind(on_dismiss=self.__init__)
+        popup.open()
+
+    def on_win(self):
+        self.timeCountdownEvent.cancel()
+        popup = Factory.WinPopup()
+        popup.bind(on_dismiss=self.__init__)
+        popup.open()
 
 
 class Menu(Screen):
